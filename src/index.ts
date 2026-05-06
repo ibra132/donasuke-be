9cb00e96e57b0185dc71d92b842a09708f9ee2ec
ibra132 <baimgunawan1@gmail.com>
2026-05-06 17:16:52 +0700

    add user verification route and handler for submission

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { userRoute } from "./routes/user.route";
 import { authRoute } from "./routes/auth.route";
 import { campaignRoute } from "./routes/campaign.route";
 import { donationRoute } from "./routes/donation.route";
@@ -7,6 +8,7 @@ import { withdrawalRoute } from "./routes/withdrawal.route";
 
 const app = new Hono();
 
+app.route("/api/users", userRoute);
 app.route("/api/auth", authRoute);
 app.route("/api/campaign", campaignRoute);
 app.route("/api/donation", donationRoute);
